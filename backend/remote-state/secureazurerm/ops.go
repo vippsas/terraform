1c334e6092b0979b8d27f35ca23dbf73ac76429e
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-03-19 11:22:27 +0100

    More preliminary code.

--- backend/remote-state/secureazurerm/ops.go
+++ backend/remote-state/secureazurerm/ops.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/terraform/backend"
 	"github.com/hashicorp/terraform/command/clistate"
-	"github.com/hashicorp/terraform/config/module"
 	"github.com/hashicorp/terraform/state"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -86,7 +84,6 @@ func (b *Backend) wait(doneCh <-chan struct{}, stopCtx context.Context, cancelCt
 		select {
 		case <-cancelCtx.Done():
 			log.Println("[WARN] Running operation was canceled.")
-			// if the operation was canceled, we need to return immediately
 			canceled = true
 		case <-doneCh:
 		}
@@ -97,59 +94,6 @@ func (b *Backend) wait(doneCh <-chan struct{}, stopCtx context.Context, cancelCt
 	case <-doneCh:
 	}
 	return
-}
-
-// refresh implements "terraform refresh"
-func (b *Backend) refresh(stopCtx context.Context, cancelCtx context.Context, op *backend.Operation, runningOp *backend.RunningOperation) {
-	// If we have no config module given to use, create an empty tree to avoid crashes when Terraform.Context is initialized.
-	if op.Module == nil {
-		op.Module = module.NewEmptyTree()
-	}
-
-	// Get our context
-	tfCtx, opState, err := b.context(op)
-	if err != nil {
-		runningOp.Err = err
-		return
-	}
-
-	// Set our state
-	runningOp.State = opState.State()
-	if runningOp.State.Empty() || !runningOp.State.HasResources() {
-		if b.CLI != nil {
-			b.CLI.Output(b.Colorize().Color("[reset][bold][yellow]Empty remote state.[reset][yellow]\n"))
-		}
-	}
-
-	// Perform the refresh in a goroutine so we can be interrupted
-	var newState *terraform.State
-	var refreshErr error
-	doneCh := make(chan struct{})
-	go func() {
-		defer close(doneCh)
-		newState, refreshErr = tfCtx.Refresh()
-	}()
-
-	if b.wait(doneCh, stopCtx, cancelCtx, tfCtx, opState) {
-		return
-	}
-
-	// Write the resulting state to the running operation.
-	runningOp.State = newState
-	if refreshErr != nil {
-		runningOp.Err = errwrap.Wrapf("Error refreshing state: {{err}}", refreshErr)
-		return
-	}
-
-	// Save state to storage account.
-	if err := opState.WriteState(newState); err != nil {
-		runningOp.Err = errwrap.Wrapf("Error writing state: {{err}}", err)
-		return
-	}
-	if err := opState.PersistState(); err != nil {
-		runningOp.Err = errwrap.Wrapf("Error saving state: {{err}}", err)
-		return
-	}
 }
 
 // terraform plan
