a5b62479515c5fd814565342caddc8170bcc5dd9
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-03-19 15:04:26 +0100

    Random refactorings.

--- backend/remote-state/secureazurerm/cli.go
+++ backend/remote-state/secureazurerm/cli.go
@@ -2,26 +2,31 @@ package secureazurerm
 
 import (
 	"github.com/hashicorp/terraform/backend"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/mitchellh/cli"
 	"github.com/mitchellh/colorstring"
 )
 
+// CLI shit.
+type CLI struct {
+	CLI         cli.Ui
+	CLIColor    *colorstring.Colorize
+	ContextOpts *terraform.ContextOpts
+	// never ask for input. always validate. always run in automation.
+}
+
 // CLIInit inits CLI.
 func (b *Backend) CLIInit(opts *backend.CLIOpts) error {
-	b.CLI = opts.CLI                 // neckbeard cli.
-	b.CLIColor = opts.CLIColor       // i <3 my colors.
-	b.ContextOpts = opts.ContextOpts // muh context.
-
-	b.OpInput = opts.Input           // i want user input or not.
-	b.OpValidation = opts.Validation // i want to validate or not.
-
-	b.RunningInAutomation = opts.RunningInAutomation // i am running in automation tool with no user interaction or not.
+	b.cli.CLI = opts.CLI                 // neckbeard cli.
+	b.cli.CLIColor = opts.CLIColor       // i <3 my colors.
+	b.cli.ContextOpts = opts.ContextOpts // muh context.
 	return nil
 }
 
 // Colorize makes the CLI output colored text.
-func (b *Backend) Colorize() *colorstring.Colorize {
-	if b.CLIColor != nil {
-		return b.CLIColor
+func (cli *CLI) Colorize() *colorstring.Colorize {
+	if cli.CLIColor != nil {
+		return cli.CLIColor
 	}
 	return &colorstring.Colorize{
 		Colors:  colorstring.DefaultColors,
---
+++ backend/remote-state/secureazurerm/wait.go
@@ -0,0 +1,55 @@
+package secureazurerm
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform/state"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+// wait waits for an operation to complete, a stop signal or a cancelation signal.
+func (b *Backend) wait(doneCh <-chan struct{}, stopCtx context.Context, cancelCtx context.Context, tfCtx *terraform.Context, opState state.State) bool {
+	// Wait for the operation to finish or for us to be interrupted so we can handle it properly.
+	select {
+	case <-stopCtx.Done():
+		if b.CLI != nil {
+			b.CLI.Output("[WARN] Stopping operation...")
+		}
+
+		// Try to force a PersistState just in case the process is terminated before we can complete.
+		if err := opState.PersistState(); err != nil {
+			// We can't error out from here, but warn the user if there was an error.
+			// If this isn't transient, we will catch it again below, and
+			// attempt to save the state another way.
+			if b.CLI != nil {
+				b.CLI.Error(fmt.Sprintf(earlyStateWriteErrorFmt, err))
+			}
+		}
+
+		// Stop the operation.
+		go tfCtx.Stop()
+		select {
+		case <-cancelCtx.Done():
+			if b.CLI != nil {
+				b.CLI.Output("[WARN] Running operation was canceled.")
+			}
+			return true
+		case <-doneCh:
+		}
+	case <-cancelCtx.Done(): // Don't call without first attempting to stop the operation.
+		if b.CLI != nil {
+			b.CLI.Output("[ERROR] Running operation was canceled without stopping.")
+		}
+		return true
+	case <-doneCh:
+	}
+	return false
+}
+
+const earlyStateWriteErrorFmt = `Error saving current state: %s
+
+Terraform encountered an error attempting to save the state before stopping
+the current operation. Once the operation is complete another attempt will be
+made to save the final state.
+`
