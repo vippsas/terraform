e2751e72fbcd58197ab2c44ef8174a262c201ff4
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-03-19 11:08:55 +0100

    Refactorings.

--- backend/remote-state/secureazurerm/client.go
+++ backend/remote-state/secureazurerm/client.go
@@ -2,8 +2,6 @@ package secureazurerm
 
 import (
 	"bytes"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io"
 
@@ -209,46 +207,4 @@ func (c *Client) isLeased() error {
 		return fmt.Errorf("no lease has been acquired on blob")
 	}
 	return nil
-}
-
-const lockinfo = "lockinfo" // must be lower case!
-
-// readLockInfo reads lockInfo from the blob's metadata.
-func (c *Client) readLockInfo() (*state.LockInfo, error) {
-	blobRef := c.getBlobRef()
-
-	// Get base64-encoded lockInfo from the blob's metadata.
-	if err := blobRef.GetMetadata(&storage.GetBlobMetadataOptions{}); err != nil {
-		return nil, fmt.Errorf("error getting blob metadata: %s", err)
-	}
-	lockInfoInBase64 := blobRef.Metadata[lockinfo]
-	if lockInfoInBase64 == "" {
-		return nil, fmt.Errorf("blob metadata %q was empty", lockinfo)
-	}
-
-	// Decode and unmarshal back to lockInfo-struct.
-	lockInfoInJSON, err := base64.StdEncoding.DecodeString(lockInfoInBase64)
-	if err != nil {
-		return nil, fmt.Errorf("error decoding base64: %s", err)
-	}
-	lockInfo := &state.LockInfo{}
-	if err = json.Unmarshal(lockInfoInJSON, lockInfo); err != nil {
-		return nil, fmt.Errorf("error unmarshalling lock info from JSON: %s", err)
-	}
-
-	return lockInfo, nil
-}
-
-// writeLockInfo writes lockInfo to the blob's metadata.
-func (c *Client) writeLockInfo(info *state.LockInfo) error {
-	blobRef := c.getBlobRef()
-	if err := blobRef.GetMetadata(&storage.GetBlobMetadataOptions{LeaseID: c.leaseID}); err != nil {
-		return fmt.Errorf("error getting metadata: %s", err)
-	}
-	if info == nil {
-		delete(blobRef.Metadata, lockinfo)
-	} else {
-		blobRef.Metadata[lockinfo] = base64.StdEncoding.EncodeToString(info.Marshal())
-	}
-	return blobRef.SetMetadata(&storage.SetBlobMetadataOptions{LeaseID: c.leaseID})
 }
