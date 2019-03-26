1aa21a0680b4b605ba68f84cf2f32eec43721d98
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-03-26 10:56:11 +0100

    Moar preliminary code.

--- backend/remote-state/secureazurerm/remote/keyvault.go
+++ backend/remote-state/secureazurerm/remote/keyvault.go
@@ -6,3 +6,8 @@ type KeyVault struct {
 func New() KeyVault {
 	return KeyVault{}
 }
+
+// Insert inserts a secret into the key vault. Returns the version.
+func (k *KeyVault) Insert(secret string) string {
+	return ""
+}
