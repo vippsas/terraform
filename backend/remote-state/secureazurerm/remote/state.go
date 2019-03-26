27c060f9f75f303481ce8c84fe55fe86a76d07eb
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-03-26 09:51:30 +0100

    Xtreme refactoring.

--- backend/remote-state/secureazurerm/remote/state.go
+++ backend/remote-state/secureazurerm/remote/state.go
@@ -25,49 +25,6 @@ type State struct {
 	modules []Module // contains what attributes are sensitive.
 }
 
-// Module is used to report which attributes are sensitive or not.
-type Module struct {
-	Path      []string
-	Resources map[string]map[string]bool
-}
-
-// secretAttribute is a sensitive attribute that is located as a secret in the Azure key vault.
-type secretAttribute struct {
-	Name    string // Name of the secret.
-	Version string // Version of the secret.
-}
-
-// interpAttr is a sensitive attribute interpolated from somewhere.
-type interpAttr struct {
-	Type      string `json: "type"`      // Type of resource.
-	ID        string `json: "id"`        // ID of the resource.
-	Attribute string `json: "attribute"` // Attribute name of resource.
-}
-
-/*
-// mask masks a sensitive attribute.
-func mask(attr string) interface{} {
-	if attr != "" {
-		return interpAttr{Attribute: attr}
-	}
-	return interpAttr{Attribute: ""}
-}
-
-// unmask unmasks a masked sensitive attribute.
-func unmask(attr interface{}) (string, error) {
-	if s, ok := attr.(string); ok {
-		return s, nil
-	}
-	if attr, ok := attr.(interpAttr); ok {
-		return "", nil
-	}
-	if attr, ok := attr.(secretAttr); ok {
-		return "", nil
-	}
-	return "", fmt.Errorf("error unmasking attributes")
-}
-*/
-
 // State reads the state from the memory.
 func (s *State) State() *terraform.State {
 	s.mu.Lock()
@@ -158,42 +115,6 @@ func (s *State) RefreshState() error {
 	return nil
 }
 
-// pathEqual compares if the path of two modules are equal.
-func pathEqual(a []interface{}, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i, val := range a {
-		if val.(string) != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-// maskModule masks all sensitive attributes in a module.
-func (s *State) maskModule(moduleIndex int, module map[string]interface{}) {
-	for resourceName, resource := range module["resources"].(map[string]interface{}) {
-		fmt.Printf("%s:\n", resourceName)
-		r := resource.(map[string]interface{})
-		primary := r["primary"].(map[string]interface{})
-		s.maskResource(moduleIndex, resourceName, primary["attributes"].(map[string]interface{}))
-	}
-}
-
-// maskResource masks all sensitive attributes in a resource.
-func (s *State) maskResource(moduleIndex int, resourceName string, attributes map[string]interface{}) {
-	for name, value := range attributes {
-		if s.modules[moduleIndex].Resources[resourceName][name] {
-			attributes[name] = secretAttribute{
-				Name:    "NameTest",
-				Version: "VerTest",
-			}
-			fmt.Printf("  %s: %v\n", name, value)
-		}
-	}
-}
-
 // PersistState saves the in-memory state to the blob.
 func (s *State) PersistState() error {
 	// Lock, harr!
@@ -252,39 +173,4 @@ func (s *State) Lock(info *state.LockInfo) (string, error) {
 // Unlock unlocks the state.
 func (s *State) Unlock(id string) error {
 	return s.Blob.Unlock(id)
-}
-
-// Report is used to report sensitive attributes to the state.
-func (s *State) Report(modules []*terraform.ModuleDiff) {
-	// Lock!
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	// Report sensitive attributes.
-	if len(s.modules) != len(modules) {
-		s.modules = make([]Module, len(modules))
-	}
-	for i, module := range modules {
-		s.modules[i].Path = make([]string, len(module.Path))
-		copy(s.modules[i].Path, module.Path)
-		s.modules[i].Resources = make(map[string]map[string]bool)
-		for resourceName, resourceValue := range module.Resources {
-			s.modules[i].Resources[resourceName] = make(map[string]bool)
-			for attrName, attrValue := range resourceValue.Attributes {
-				s.modules[i].Resources[resourceName][attrName] = attrValue.Sensitive
-			}
-		}
-	}
-	// DEBUG: Print which attributes are sensitive. ~ bao.
-	/*
-		for i, module := range s.modules {
-			fmt.Printf("Module: %d\n", i)
-			for name, resource := range module.Resources {
-				fmt.Printf("Resource: %s:\n", name)
-				for attribute, value := range resource {
-					fmt.Printf("  %s: %t\n", attribute, value)
-				}
-			}
-		}
-	*/
 }
