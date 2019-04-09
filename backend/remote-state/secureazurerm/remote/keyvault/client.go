22fa92c37e633778a5e413aeb23a5ab1f11bb001
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-04-09 14:00:41 +0200

    Implemented clearer error messages.

--- backend/remote-state/secureazurerm/remote/keyvault/client.go
+++ backend/remote-state/secureazurerm/remote/keyvault/client.go
@@ -29,22 +29,22 @@ type KeyVault struct {
 }
 
 // generateKeyVaultName generates a new random key vault name of max length.
-func generateKeyVaultName() string {
+func generateKeyVaultName() (string, error) {
 	var a, b string
 	a, err := rand.GenLowerAlphas(1)
 	if err != nil {
-		panic(err)
+		return "", fmt.Errorf("error generating alphabetic characters: %s", err)
 	}
 	b, err = rand.GenLowerAlphanums(23)
 	if err != nil {
-		panic(err)
+		return "", fmt.Errorf("error generating lowercase alphabetic and numeric characters: %s", err)
 	}
-	return a + b
+	return a + b, nil
 }
 
 // Setup creates a new Azure Key Vault.
-func Setup(ctx context.Context, props *properties.Properties, workspace string) (KeyVault, error) {
-	k := KeyVault{
+func Setup(ctx context.Context, props *properties.Properties, workspace string) (*KeyVault, error) {
+	k := &KeyVault{
 		resourceGroupName: props.ResourceGroupName,
 		vaultClient:       keyvault.NewVaultsClient(props.SubscriptionID),
 		keyClient:         KV.New(),
@@ -57,10 +57,13 @@ func Setup(ctx context.Context, props *properties.Properties, workspace string) 
 	// TODO: Replace these by saving the key vault name in the state itself.
 	group, err := props.GroupsClient.Get(props.ResourceGroupName)
 	if err != nil {
-		return k, err
+		return nil, fmt.Errorf("error getting resource group named %s: %s", props.ResourceGroupName, err)
 	}
 	if group.Tags == nil {
-		k.vaultName = generateKeyVaultName()
+		k.vaultName, err = generateKeyVaultName()
+		if err != nil {
+			return nil, fmt.Errorf("error generating key vault name: %s", err)
+		}
 
 		tags := make(map[string]*string)
 		tags[workspace] = &k.vaultName
@@ -75,7 +78,10 @@ func Setup(ctx context.Context, props *properties.Properties, workspace string) 
 			return k, fmt.Errorf("error updating tags on resource group %s: %s", props.ResourceGroupName, err)
 		}
 	} else if (*(group.Tags))[workspace] == nil {
-		k.vaultName = generateKeyVaultName()
+		k.vaultName, err = generateKeyVaultName()
+		if err != nil {
+			return nil, fmt.Errorf("error generating key vault name: %s", err)
+		}
 
 		(*group.Tags)[workspace] = &k.vaultName
 		_, err = props.GroupsClient.CreateOrUpdate(
@@ -86,7 +92,7 @@ func Setup(ctx context.Context, props *properties.Properties, workspace string) 
 			},
 		)
 		if err != nil {
-			return k, fmt.Errorf("error updating tags on resource group %s: %s", props.ResourceGroupName, err)
+			return nil, fmt.Errorf("error updating tags on resource group %s: %s", props.ResourceGroupName, err)
 		}
 	} else {
 		k.vaultName = *(*group.Tags)[workspace]
@@ -97,7 +103,7 @@ func Setup(ctx context.Context, props *properties.Properties, workspace string) 
 	if err != nil {
 		tenantID, err := uuid.FromString(props.TenantID)
 		if err != nil {
-			return k, fmt.Errorf("error converting tenant ID-string to UUID: %s", err)
+			return nil, fmt.Errorf("error converting tenant ID-string to UUID: %s", err)
 		}
 		vault, err = k.vaultClient.CreateOrUpdate(ctx, props.ResourceGroupName, k.vaultName, keyvault.VaultCreateOrUpdateParameters{
 			Location: to.StringPtr(props.Location),
@@ -124,14 +130,14 @@ func Setup(ctx context.Context, props *properties.Properties, workspace string) 
 			},
 		})
 		if err != nil {
-			return k, fmt.Errorf("error creating key vault: %s", err)
+			return nil, fmt.Errorf("error creating key vault: %s", err)
 		}
 	}
 	k.vaultURI = *vault.Properties.VaultURI
 
 	k.keyClient.Authorizer, err = auth.NewVault()
 	if err != nil {
-		return k, err
+		return nil, fmt.Errorf("error creating new vault authorizer: %s", err)
 	}
 	return k, nil
 }
