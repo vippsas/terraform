4c5f9fd85be8d4b14b597ace30e735deccd53fe6
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-04-12 09:41:11 +0200

    Simplified CLI colored output code.

--- backend/remote-state/secureazurerm/apply.go
+++ backend/remote-state/secureazurerm/apply.go
@@ -168,16 +168,13 @@ func (b *Backend) apply(stopCtx context.Context, cancelCtx context.Context, op *
 	// If we have a UI, output the results
 	if b.CLI != nil {
 		if op.Destroy {
-			b.CLI.Output(b.Colorize().Color(fmt.Sprintf(
-				"[reset][bold][green]\n%d destroyed.\n\nDestroy complete!",
-				countHook.Removed)))
+			b.ColorOutput(
+				fmt.Sprintf("[reset][bold][green]\n%d destroyed.\n\nDestroy complete!",
+					countHook.Removed))
 		} else {
-			b.CLI.Output(b.Colorize().Color(fmt.Sprintf(
-				"[reset][bold][green]\n%d added, %d changed, %d destroyed.\n\nApply complete!",
-				countHook.Added,
-				countHook.Changed,
-				countHook.Removed)))
-			// TODO: Output state file.
+			b.ColorOutput(
+				fmt.Sprintf("[reset][bold][green]\n%d added, %d changed, %d destroyed.\n\nApply complete!",
+					countHook.Added, countHook.Changed, countHook.Removed))
 		}
 	}
 }
