f61db5314e9d1d545317c47dd073db9b98e57b6c
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2019-04-24 19:00:50 +0200

    Removed MSI authentication. It was untested after all. Don't know if it works.

--- backend/remote-state/secureazurerm/remote/auth/auth.go
+++ backend/remote-state/secureazurerm/remote/auth/auth.go
@@ -17,44 +17,29 @@ func NewMgmt() (props properties.Properties, err error) {
 	// Try authorizing using Azure CLI, which will use the resource: https://management.azure.com/.
 	props.MgmtAuthorizer, err = auth.NewAuthorizerFromCLIWithResource(azure.PublicCloud.ResourceManagerEndpoint)
 	if err != nil {
-		// Fetch subscriptionID from environment variable AZURE_SUBSCRIPTION_ID.
-		settings, innerErr := auth.GetSettingsFromEnvironment()
-		if err != nil {
-			err = fmt.Errorf("error creating new authorizer from CLI: %v: error getting settings from environment: %v", err, innerErr)
-			return
-		}
-		props.SubscriptionID = settings.GetSubscriptionID()
-		if props.SubscriptionID == "" {
-			err = fmt.Errorf("error creating new authorizer from CLI: %v: environment variable %v is not set", err, auth.SubscriptionID)
-			return
-		}
+		err = fmt.Errorf("error creating new authorizer from CLI: %v", err)
+		return
+	}
+	// Fetch subscriptionID and tenantID from Azure CLI.
+	var out []byte
+	out, err = exec.Command("az", "account", "show", "--output", "json").Output()
+	if err != nil {
+		err = fmt.Errorf("error fetching subscription id using Azure CLI: %s", err)
+		return
+	}
+	var m map[string]interface{}
+	if err = json.Unmarshal(out, &m); err != nil {
+		err = fmt.Errorf("error unmarshalling subscription ID and tenant ID from JSON output from Azure CLI: %s", err)
+		return
+	}
+	props.SubscriptionID = m["id"].(string)
+	props.TenantID = m["tenantId"].(string)
 
-		// Authorize using MSI.
-		props.MgmtAuthorizer, innerErr = settings.GetMSI().Authorizer()
-		if innerErr != nil {
-			err = fmt.Errorf("error creating new authorizer from CLI: %v: error creating authorizer from environment: %v", err, innerErr)
-			return
-		}
-	} else {
-		// Fetch subscriptionID from Azure CLI.
-		var out []byte
-		out, err = exec.Command("az", "account", "show", "--output", "json").Output()
-		if err != nil {
-			err = fmt.Errorf("error fetching subscription id using Azure CLI: %s", err)
-			return
-		}
-		var m map[string]interface{}
-		if err = json.Unmarshal(out, &m); err != nil {
-			err = fmt.Errorf("error unmarshalling subscription ID and tenant ID from JSON output from Azure CLI: %s", err)
-			return
-		}
-		props.SubscriptionID = m["id"].(string)
-		props.TenantID = m["tenantId"].(string)
-		out, err = exec.Command("az", "ad", "signed-in-user", "show", "--output", "json", "--query", "objectId").Output()
-		if err = json.Unmarshal(out, &props.ObjectID); err != nil {
-			err = fmt.Errorf("error unmarshalling object ID from JSON output from Azure CLI: %s", err)
-			return
-		}
+	// Get the objectID of the signed-in user.
+	out, err = exec.Command("az", "ad", "signed-in-user", "show", "--output", "json", "--query", "objectId").Output()
+	if err = json.Unmarshal(out, &props.ObjectID); err != nil {
+		err = fmt.Errorf("error unmarshalling object ID from JSON output from Azure CLI: %s", err)
+		return
 	}
 	err = nil
 	return
