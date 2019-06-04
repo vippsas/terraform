193a8c4ced6bb7a8b0f8676b2b055cd00703409d
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2019-06-04 15:07:41 +0200

    Simplified authentication code.

--- backend/remote-state/secureazurerm/backend.go
+++ backend/remote-state/secureazurerm/backend.go
@@ -2,16 +2,20 @@ package secureazurerm
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
 	"strings"
 	"sync"
 
 	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
+	"github.com/Azure/go-autorest/autorest/azure"
+	azauth "github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/hashicorp/terraform/backend"
 	"github.com/hashicorp/terraform/backend/remote-state/secureazurerm/properties"
 	"github.com/hashicorp/terraform/backend/remote-state/secureazurerm/remote/account"
-	"github.com/hashicorp/terraform/backend/remote-state/secureazurerm/remote/auth"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/mitchellh/cli"
 	"github.com/mitchellh/colorstring"
@@ -67,9 +71,49 @@ func New() backend.Backend {
 // configure bootstraps the Azure resources needed to use this backend.
 func (b *Backend) configure(ctx context.Context) error {
 	var err error
-	b.props, err = auth.NewMgmt()
+
+	// Try authorizing using Azure CLI, which will use the resource: https://management.azure.com/.
+	b.props.MgmtAuthorizer, err = azauth.NewAuthorizerFromCLIWithResource(azure.PublicCloud.ResourceManagerEndpoint)
 	if err != nil {
-		return fmt.Errorf("error creating new mgmt authorizer: %s", err)
+		return fmt.Errorf("error creating new authorizer from CLI with resource %s: %v", azure.PublicCloud.ResourceManagerEndpoint, err)
+	}
+	// Fetch subscriptionID and tenantID from Azure CLI.
+	var out []byte
+	out, err = exec.Command("az", "account", "show", "--output", "json").Output()
+	if err != nil {
+		return fmt.Errorf("error fetching subscription id using Azure CLI: %s", err)
+	}
+	var m map[string]interface{}
+	if err = json.Unmarshal(out, &m); err != nil {
+		return fmt.Errorf("error unmarshalling subscription ID and tenant ID from JSON output from Azure CLI: %s", err)
+	}
+	b.props.SubscriptionID = m["id"].(string)
+	b.props.TenantID = m["tenantId"].(string)
+	user := m["user"].(map[string]interface{})
+
+	// Get the objectID of the signed-in user.
+	userType := user["type"].(string)
+	switch userType {
+	case "servicePrincipal":
+		clientID := user["name"].(string)
+		out, err = exec.Command("az", "ad", "sp", "show", "--id", clientID, "--output", "json", "--query", "objectId").Output()
+		if err != nil {
+			return fmt.Errorf("error getting service principal: %s", err)
+		}
+		os.Setenv("ARM_CLIENT_ID", clientID)
+		os.Setenv("ARM_CLIENT_SECRET", os.Getenv("servicePrincipalKey")) // defined in the agent after enabling a setting.
+		os.Setenv("ARM_SUBSCRIPTION_ID", b.props.SubscriptionID)
+		os.Setenv("ARM_TENANT_ID", b.props.TenantID)
+	case "user":
+		out, err = exec.Command("az", "ad", "signed-in-user", "show", "--output", "json", "--query", "objectId").Output()
+		if err != nil {
+			return fmt.Errorf("error getting signed-in user: %s", err)
+		}
+	default:
+		return fmt.Errorf("unknown user type")
+	}
+	if err = json.Unmarshal(out, &b.props.ObjectID); err != nil {
+		return fmt.Errorf("error unmarshalling object ID from JSON output from Azure CLI: %s", err)
 	}
 
 	// Get the data attributes from the "backend"-block.
