a960ab9f91dab0f4ab2e249c5f202328dc4eba81
Bao Chi Tran Nguyen <bao.chi.tran.nguyen@vipps.no>
2020-03-11 10:36:36 +0100

    Removed the version check, since it's always going to print outdated due to this being a fork.

--- command/version.go
+++ command/version.go
@@ -95,24 +95,6 @@ func (c *VersionCommand) Run(args []string) int {
 		}
 	}
 
-	// If we have a version check function, then let's check for
-	// the latest version as well.
-	if c.CheckFunc != nil {
-
-		// Check the latest version
-		info, err := c.CheckFunc()
-		if err != nil {
-			c.Ui.Error(fmt.Sprintf(
-				"\nError checking latest version: %s", err))
-		}
-		if info.Outdated {
-			c.Ui.Output(fmt.Sprintf(
-				"\nYour version of Terraform is out of date! The latest version\n"+
-					"is %s. You can update by downloading from www.terraform.io/downloads.html",
-				info.Latest))
-		}
-	}
-
 	return 0
 }
 
